b0baa4dc795b0ddb33771a5645f4d887edb65c90
nerdykamil <yusra_shahid95@hotmail.com>
2018-10-26 02:50:20 +0500

    analyzedusing ggplot2

--- chapter2/analyzing_automobile_efficiency_over_time.R
+++ chapter2/analyzing_automobile_efficiency_over_time.R
@@ -78,3 +78,59 @@ gasCars <- subset(vehicles, fuelType1 %in% c("Regular Gasoline", "Premium Gasoli
                   & fuelType2 == "" & atvType != "Hybrid")
 mpgByYr_Gas <- ddply(gasCars,~year,summarize,avgMPG = mean(comb08),avgHghy = mean(highway08), avgCity = mean(city08))
 ggplot(mpgByYr_Gas, aes(year,avgMPG)) + geom_point() + geom_smooth() + xlab("Year") + ylab("Average MPG") + ggtitle("Gasoline Cars")
+
+# finding why the curve is increasing towards end
+# do we have fewer large engine cars now
+typeof(gasCars$displ) #assuming gasCars$displ value is a char
+gasCars$displ <- as.numeric(gasCars$displ) #converting it to numeric
+
+ggplot(gasCars, aes(displ,comb08)) + geom_point() + geom_smooth() + xlab("displ") + ylab("comb08") + ggtitle("Gasoline Cars engine displacement")
+## thus plot from above line tells us that small cars have greter fuel efficiency
+
+avgCarSize <- ddply (gasCars, ~year, summarize , avgDispl = mean(displ))
+ggplot(avgCarSize, aes(year,avgDispl)) + geom_point() + geom_smooth() + xlab("Year") + ylab("Avg displ of engine") + ggtitle("car displ over years")
+
+## plotting avg engine displacement and fuel efficiency on one plot
+byYear <- ddply(gasCars, ~year, summarise, avgMPG = mean(comb08), avgDispl = mean(displ))
+head(byYear)
+
+# melting data frame to convert from wide to long format (columns year, avgdispl, 
+# avgmpg converted to columns year, variable, value )
+# where variable column shows if its avgdispl value or avgmpg value
+byYear2 <- melt(byYear, id = "year")
+head(byYear2)
+levels(byYear$varianble) <- c("Average MPG", "Avg displ")
+
+#use nrow to compare rows of byYear and byYear2
+nrow(byYear)
+nrow(byYear2)
+
+# plot using ggplot (the plot below helps in identifying trends between carsize and avgMPG over years)
+ggplot(byYear2, aes(year,value)) + geom_point() + geom_smooth() + facet_wrap(~variable, ncol =1, scales ="free_y") + xlab("Year") + ylab("")
+
+# finding efficiencies of manual and automatic cars for four cylinder engines
+gasCars4 <- subset(gasCars, cylinders == "4")
+
+# the below plot is used to generate a boxplot of values that 
+#show the distribution of values
+ggplot(gasCars4, aes(factor(year),comb08)) + geom_boxplot() + facet_wrap(~trany2,ncol=1) + theme(axis.text.x = element_text(angle = 45)) + labs(x="Year",y = "MPG")
+
+# now lets see the change in proportion of manual cars available each year
+ggplot(gasCars4, aes(factor(year),fill = factor(trany2))) + geom_bar(position = "fi-ll")+ labs(x="Year",y="Propotion of cars",fill= "Transmission") + theme(axis.text.x = element_text(angle = 45)) + geom_hline(yintercept = 0.5,linetype=2)
+
+## investigating the makes and models of cars with four cylinders
+carsMake <- ddply(gasCars4,~year,summarise,numberOfMakes = length(unique(make)))
+
+ggplot(carsMake, aes(year,numberOfMakes)) + geom_point() +labs(x = "Year",y = "number of available makes") + ggtitle( "Four cylinder cars")
+
+# finding makes available every years
+uniqMakes <- dlply(gasCars4,~year,function(x) unique(x$make))
+# finding constants in every year
+commonMakes <- Reduce(intersect,uniqMakes) #reduce is a higher order func and accepts another func as input
+commonMakes
+
+# finding fuel efficiency of these makers over time
+carsCommonMakes4 <- subset(gasCars4, make %in% commonMakes)
+avgMPG_commonMakes <- ddply(carsCommonMakes4, ~year+make,summarise,avgMPG = mean(comb08))
+
+ggplot(avgMPG_commonMakes, aes(year,avgMPG)) + geom_line()+facet_wrap(~make,nrow =2)
